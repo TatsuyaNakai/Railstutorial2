3acc1b2af8229993658ecacbf86445b6518be22e
TatsuyaNakai <15g079nt@gmail.com>
2021-08-16 02:15:12 +0000

    Finish layout and routes

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    # ?マークにコンマの次の文字列を代入してる。
+    # assert_selectは、第一引数があるかどうかをテストしてる。あれば通過する。
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    get signup_path
+    assert_select "title", full_title()
+  end
+  
+  
+end
