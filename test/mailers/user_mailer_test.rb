931f22b575d08fd629457a9782c26385a5cd83c4
TatsuyaNakai <15g079nt@gmail.com>
2021-08-20 00:40:59 +0000

    Add password reset

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -13,11 +13,24 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match user.activation_token,   mail.body.encoded
     assert_match CGI.escape(user.email),  mail.body.encoded
   end
-end
-
 # encoredメソッド。本来の使い方は、引数を指定して、
 # その引数を文字コードに変化するのが、使い方
 # けど、テストの時は、期待値を文字コードとして認識する。
 
-# 今回の場合でいうと、期待値を文字コードに変えたものがmailのbodyに
-# あるか（一致するか）を検証してる。
+# 今回の場合でいうと、期待値を文字コードに変えたものがmailのどこか
+# bodyはbodu部分じゃない。HTML全体の話。
+# にあるか（一致するか）を検証してる。
+
+
+  test "password_reset" do
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "Password reset", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.reset_token,        mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
+  end
+
+end
